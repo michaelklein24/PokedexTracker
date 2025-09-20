a3619d147556edf074045de0d0960a15f2fcf591
Michael Klein <michaelklein1997@gmail.com>
2025-09-19 20:08:26 -0500

    initial commit

---
+++ server/src/main/java/com/klein/pokedextracker/PokedextrackerApplication.java
@@ -0,0 +1,13 @@
+package com.klein.pokedextracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedextrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokedextrackerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/klein/pokedextracker/PokedextrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klein.pokedextracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedextrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
